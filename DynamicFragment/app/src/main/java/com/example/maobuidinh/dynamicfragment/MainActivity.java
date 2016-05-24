772f694605774f8328b26fbd4f675300e03c27c5
maobui <mao.buidinh@hotmail.com>
2016-05-24 15:33:42 +0700

    - Test git command,

--- DynamicFragment/app/src/main/java/com/example/maobuidinh/dynamicfragment/MainActivity.java
+++ DynamicFragment/app/src/main/java/com/example/maobuidinh/dynamicfragment/MainActivity.java
@@ -32,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically by the system.
         setContentView(R.layout.fragment_layout);
 
-        //Test for Git commit.
+        //Test for Git commit command.
     }
 }

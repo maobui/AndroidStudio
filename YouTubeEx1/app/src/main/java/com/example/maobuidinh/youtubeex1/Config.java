fca8c3f49a6f3f6f133d379f55845003a0ef742c
mao.buidinh <mao.buidinh@hotmail.com>
2017-05-13 12:57:01 +0700

    + add prj example youtube api.

---
+++ YouTubeEx1/app/src/main/java/com/example/maobuidinh/youtubeex1/Config.java
@@ -0,0 +1,14 @@
+package com.example.maobuidinh.youtubeex1;
+
+/**
+ * Created by maobuidinh on 5/13/2017.
+ */
+
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyABYoczeHg4XABx_jMRfv-CqmA2YMsIY4A";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}

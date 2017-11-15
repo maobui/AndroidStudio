f9ad0cf7efae136aca471b8c11e12a75f04501a9
mao.buidinh <mao.buidinh@hotmail.com>
2017-11-15 23:25:53 +0700

    - add livedata ex.

---
+++ app/src/main/java/com/me/bui/livedata/MainActivity.java
@@ -0,0 +1,30 @@
+package com.me.bui.livedata;
+
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+public class MainActivity extends LifecycleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.list);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.VISIBLE);
+        MainActivityViewModel model = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        model.getFruitList().observe(this, fruitlist -> {
+            // update UI
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_list_item_1, android.R.id.text1, fruitlist);
+            // Assign adapter to ListView
+            listView.setAdapter(adapter);
+            progressBar.setVisibility(View.GONE);
+        });
+    }
+}
---
+++ app/src/main/java/com/me/bui/livedata/MainActivityViewModel.java
@@ -0,0 +1,54 @@
+package com.me.bui.livedata;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by mao.bui on 11/15/2017.
+ */
+
+public class MainActivityViewModel extends ViewModel {
+    private static final String TAG = MainActivityViewModel.class.getSimpleName();
+
+    private MutableLiveData<List<String>> fruitList;
+
+    LiveData<List<String>> getFruitList() {
+        if (fruitList == null) {
+            fruitList = new MutableLiveData<>();
+            loadFruits();
+        }
+        return fruitList;
+    }
+
+    private void loadFruits() {
+        // do async operation to fetch users
+        Handler myHandler = new Handler();
+        myHandler.postDelayed(() -> {
+            List<String> fruitsStringList = new ArrayList<>();
+            fruitsStringList.add("Mango");
+            fruitsStringList.add("Apple");
+            fruitsStringList.add("Orange");
+            fruitsStringList.add("Banana");
+            fruitsStringList.add("Grapes");
+            long seed = System.nanoTime();
+            Collections.shuffle(fruitsStringList, new Random(seed));
+
+            fruitList.setValue(fruitsStringList);
+        }, 5000);
+
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.d(TAG, "on cleared called");
+    }
+}

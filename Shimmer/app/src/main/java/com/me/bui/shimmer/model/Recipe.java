22a3be7e505079405e01eca8f499fd39c726ca3b
mao.buidinh <mao.buidinh@hotmail.com>
2018-03-15 00:07:04 +0700

    - update prj Shimmer.

---
+++ Shimmer/app/src/main/java/com/me/bui/shimmer/model/Recipe.java
@@ -0,0 +1,46 @@
+package com.me.bui.shimmer.model;
+
+/**
+ * Created by mao.bui on 3/14/2018.
+ */
+
+public class Recipe {
+    int id;
+    String name;
+    String description;
+    double price;
+    String thumbnail;
+    String chef;
+    String timestamp;
+
+    public Recipe() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public String getChef() {
+        return chef;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+}
